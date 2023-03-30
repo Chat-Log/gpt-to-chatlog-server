61d02b570b341d0a961101330cc0d7b2df946683
constwon111 <constwon111@gmail.com>
2023-03-30 13:18:11 +0900

    [Chore] chat gpt ai 모델 테스트 라우터

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { makeCompletion } from './ai-models/gpt-model';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +9,30 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    console.log('hi');
     return this.appService.getHello();
+  }
+
+  @Get('/test')
+  async getHello2(
+    @Query('chat') chat: string,
+    @Res() res: Response,
+  ): Promise<string> {
+    console.log(chat);
+    makeCompletion(chat, res);
+    // console.log('hi');
+
+    return 'hi';
+    // return this.appService.getHello();
+  }
+  @Post('/test')
+  async getHello3(@Body() body: any, @Res() res: Response): Promise<string> {
+    console.log(body.chat);
+    // console.log(chat);
+    makeCompletion(body.chat, res);
+    // console.log('hi');
+
+    return 'hi';
+    // return this.appService.getHello();
   }
 }
