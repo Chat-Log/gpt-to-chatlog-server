0e513725c2dde88444d704245213e08597cfd789
constwon111 <constwon111@gmail.com>
2023-04-03 16:16:56 +0900

    [Feat] 질문하는 라우터의 dto

---
+++ src/topic/infra/dto/ask-question.dto.ts
@@ -0,0 +1,34 @@
+import {
+  IsArray,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Model } from 'src/topic/domain/enum/model';
+
+export class AskQuestionDto {
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
+
+  @IsEnum(Model)
+  @IsNotEmpty()
+  model: Model;
+
+  @IsNotEmpty()
+  @IsString()
+  question: string;
+
+  @IsNotEmpty()
+  @IsArray()
+  tags: string[];
+
+  @IsOptional()
+  @IsString()
+  topicId: string[];
+
+  @IsOptional()
+  @IsArray()
+  prevCompletionIds: string[];
+}
