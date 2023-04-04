616522b0fcd2490d0f8080fc089fbc83daf331b9
constwon111 <constwon111@gmail.com>
2023-04-05 01:32:27 +0900

    [Feat] 기본적인 질문, 응답시 토픽에 대한 로직 완성.

--- src/topic/infra/topic.service.ts
+++ src/topic/infra/topic.service.ts
@@ -1,13 +1,12 @@
 ﻿import { Injectable } from '@nestjs/common';
-import { CompletionImpl } from './completion/completion';
 import { ChatGPTModel } from 'src/ai-models/chat-gpt-model';
 import { TopicImpl } from './topic';
 import { Readable } from 'stream';
 import { Model } from '../../common/enum/enum';
 import { ModelProvider } from 'src/model-provider/model-provider';
 import { AskQuestionDto } from './dto/ask-question.dto';
-import { Topic } from '../domain/topic';
 import { TopicRepository } from './topic.repository';
+import { UserImpl } from '../../user/infra/user';
 
 @Injectable()
 export class TopicService {
@@ -23,37 +22,19 @@ export class TopicService {
 
   async makeCompletion(dto: AskQuestionDto): Promise<Readable> {
     const { model, question, tags, topicId, prevCompletionIds } = dto;
-    const apiModel = this.chooseModel(model);
-    const newCompletion = new CompletionImpl(apiModel, question);
-    const answerStream = CompletionImpl.askQuestion(
-      new ChatGPTModel(),
-      question,
-    );
-    answerStream.on('data', (data) => {
-      console.log(data);
-      // console.log(data.toString());
-    });
-    // console.log(answerStream.o);
-    // answerStream.on('data', (data) => {
-    //   console.log(data);
-    // });
+    let topic;
+    if (!topicId) {
+      topic = TopicImpl.createTopic([], new UserImpl());
+    } else {
+      topic = await this.topicRepository.findOne({ where: { id: topicId } });
+    }
+    const answerStream = topic.askToModel(this.chooseModel(model), question);
+
+    answerStream.on('data', (data) => {});
     answerStream.on('end', async () => {
-      newCompletion.writeAnswer('answer');
-      const topic = TopicImpl.createTopicForFirstCompletion(
-        question.slice(0, 10),
-        newCompletion,
-      );
-      // if (!topicId) {
-      //   const topic = TopicImpl.createTopicForFirstCompletion(
-      //     question.slice(0, 10),
-      //     newCompletion,
-      //   );
-      //
-      //   return answerStream;
-      // }
       await this.topicRepository.save(topic);
-      console.log(await this.topicRepository.find());
     });
+
     return answerStream;
   }
 
