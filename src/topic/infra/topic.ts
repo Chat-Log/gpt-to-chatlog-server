7b6cda8f179c7791b6579398d3f0063c74e1e79e
constwon111 <constwon111@gmail.com>
2023-04-05 01:31:04 +0900

    [Feat] topic에서 가지고 있는 completions로 질문할수 있도록 askToModel추가

--- src/topic/infra/topic.ts
+++ src/topic/infra/topic.ts
@@ -3,43 +3,78 @@ import { Completion } from '../domain/completion/completion';
 import { Topic } from '../domain/topic';
 import { TopicProps } from '../interface/interface';
 import { CompletionImpl } from './completion/completion';
-
+import { User } from '../../user/domain/user';
+import { v4 as uuid } from 'uuid';
+import { Tag } from '../domain/completion/tag/tag';
+import { Readable } from 'stream';
 export class TopicImpl implements Topic {
-  private id: number;
-  private name: string;
+  private id: string;
+  private title: string;
+  private tags: Tag[];
   private completions: Completion[] = [];
+  private user: User;
 
-  static createTopicForFirstCompletion(
-    topicName: string,
-    completion: Completion,
-  ): Topic {
+  updateTopicTitle(title: string): void {
+    this.title = title;
+  }
+
+  static createTopic(tags: Tag[], user: User): Topic {
     const newTopic = new TopicImpl();
-    newTopic.id = 10;
-    newTopic.name = '반가워요';
+    newTopic.tags = tags;
+    newTopic.id = uuid();
+    newTopic.user = user;
 
-    // const newCompletion = CompletionImpl.createCompletion(
-    //   model,
-    //   question,
-    //   prevCompletions,
-    // );
-    // newCompletion.askQuestion();
-
-    // newTopic.name = name;
     return newTopic;
   }
-  updateTopicTitle(name: string): void {
-    this.name = name;
+  private getCurrentCompletion(): Completion {
+    return this.completions[this.completions.length - 1];
+  }
+  private getPreviousCompletions(): Completion[] {
+    return this.completions.slice(0, this.completions.length - 1);
+  }
+  askToModel(modelProvider: ModelProvider, question: string): Readable {
+    this.createQuestion(modelProvider, question);
+    let answer = '';
+
+    let tokenCount = modelProvider.countToken(this.completions);
+
+    const resultStream = modelProvider.askQuestion(
+      this.getCurrentCompletion(),
+      { previousCompletions: this.getPreviousCompletions() },
+    );
+
+    resultStream.on('data', (answerToken) => {
+      answer += answerToken;
+      tokenCount += 1;
+    });
+    resultStream.on('end', () => {
+      this.reflectAnswerAndTokenCount(answer, tokenCount);
+    });
+    return resultStream;
+  }
+  private createQuestion(modelProvider: ModelProvider, question: string): void {
+    this.completions.push(
+      CompletionImpl.createQuestion(modelProvider, question),
+    );
+  }
+  reflectAnswerAndTokenCount(answer: string, tokenCount: number): void {
+    if (this.completions.length != 0) {
+      this.getCurrentCompletion().reflectAnswerAndTokenCount(
+        answer,
+        tokenCount,
+      );
+    }
+  }
+  resumeTopic(completion: Completion) {
+    this.completions.push(completion);
   }
 
   getProps(): TopicProps {
     return {
       id: this.id,
-      name: this.name,
+      title: this.title,
       completions: this.completions,
+      tags: this.tags.map((tag) => tag.getProps()),
     };
-  }
-
-  answerQuestion(completion: Completion): void {
-    this.completions.push(completion);
   }
 }
