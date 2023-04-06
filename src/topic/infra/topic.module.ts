b2637e80e9433b066ee2658956905506c473c5f9
constwon111 <constwon111@gmail.com>
2023-04-06 20:05:01 +0900

    [Feat] 추가된 topic, tag, completion 의 entity와 repository를 모듈에 등록 및 service에 주입

--- src/topic/infra/topic.module.ts
+++ src/topic/infra/topic.module.ts
@@ -5,10 +5,25 @@ import { TopicService } from './topic.service';
 import { TopicOrmRepository } from './topic.orm-repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TopicOrmEntity } from './topic.orm-entity';
+import { TagOrmEntity } from './completion/tag/tag.orm-entity';
+import { CompletionOrmEntity } from './completion/completion.orm-entity';
+import { TagOrmRepository } from './completion/tag/tag.orm-repository';
+import { CompletionOrmRepository } from './completion/completion.orm-repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TopicOrmEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      TopicOrmEntity,
+      TagOrmEntity,
+      CompletionOrmEntity,
+    ]),
+  ],
   controllers: [TopicController],
-  providers: [TopicService, TopicOrmRepository],
+  providers: [
+    TopicService,
+    TopicOrmRepository,
+    TagOrmRepository,
+    CompletionOrmRepository,
+  ],
 })
 export class TopicModule {}
