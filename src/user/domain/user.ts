591357bf851f087b9fdc25acac516ca5b73601d8
constwon111 <constwon111@gmail.com>
2023-04-06 21:00:08 +0900

    [Refactor] user의 도메인 모델의 사소한 띄워쓰기 수정 및 createdByEmail을 유스케이스를 잘 표현하기 위해 signUpByEmail()로 수정

--- src/user/domain/user.ts
+++ src/user/domain/user.ts
@@ -1,9 +1,15 @@
 export interface User {
   loginByEmail(email: string, password: string): User;
+
   loginByKakao(): User;
+
   changeApiKey(): void;
+
   logout(): void;
+
   resetPassword(): string;
+
   changePassword(): boolean;
+
   findEmail(phone: string): string;
 }
--- src/user/infra/user.ts
+++ src/user/infra/user.ts
@@ -5,28 +5,38 @@ export class UserImpl implements User {
   private password: string;
   private apiKey: string;
 
-  static createByEmail(email: string, password: string): UserImpl {
+  static signUpByEmail(
+    email: string,
+    password: string,
+    phone: string,
+  ): UserImpl {
     return new UserImpl();
   }
 
   loginByEmail(email: string, password: string): User {
     throw new Error('Method not implemented.');
   }
+
   loginByKakao(): User {
     throw new Error('Method not implemented.');
   }
+
   changeApiKey(): void {
     throw new Error('Method not implemented.');
   }
+
   logout(): void {
     throw new Error('Method not implemented.');
   }
+
   resetPassword(): string {
     throw new Error('Method not implemented.');
   }
+
   changePassword(): boolean {
     throw new Error('Method not implemented.');
   }
+
   findEmail(phone: string): string {
     throw new Error('Method not implemented.');
   }
