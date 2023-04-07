531d37b77fda0d91bcd66727e893253eb972ab0e
constwon111 <constwon111@gmail.com>
2023-04-07 13:39:56 +0900

    [Refactor] base orm repository에서 공통된 로직이 Util에 분산되어있었으므로 이를 합침. - getFindManyQuery, getFindOneBaseQuery등 혼란을 줄수 있는 여러 함수이름들이 함께 있어서, 이를 prepareFindManyQuery prepareFindOneQuery, defineRelations 와같이 의도가 잘 보이도록 바꿈

--- src/common/base.orm-repository.ts
+++ src/common/base.orm-repository.ts
@@ -5,7 +5,6 @@ import {
   IFindOneOptions,
   IFindOneResult,
 } from './interface/interface';
-import { makeFindManyQuery } from './util/util';
 
 export abstract class BaseOrmRepository<Model, Entity> {
   protected constructor(
@@ -13,15 +12,6 @@ export abstract class BaseOrmRepository<Model, Entity> {
     protected readonly mapper: BaseMapper<Model, Entity>,
   ) {}
 
-  protected abstract getFindOneQuery(
-    options: IFindOneOptions<DeepPartial<Entity>>,
-  ): SelectQueryBuilder<Entity>;
-
-  protected getFindManyQuery(
-    options: IFindOneOptions<DeepPartial<Entity>>,
-  ): SelectQueryBuilder<Entity> {
-    return makeFindManyQuery(this.getFindOneQuery(options), options);
-  }
   public async save(model: Model): Promise<Model> {
     const entity = this.mapper.toEntity(model);
     try {
@@ -35,10 +25,11 @@ export abstract class BaseOrmRepository<Model, Entity> {
       }
     }
   }
+
   public async findOne(
     options: IFindOneOptions<DeepPartial<Entity>>,
   ): Promise<Model> {
-    const query = this.getFindOneQuery(options);
+    const query = this.prepareFindOneQuery(options);
 
     return this.mapper.toModel(await query.getOne());
   }
@@ -46,7 +37,7 @@ export abstract class BaseOrmRepository<Model, Entity> {
   public async find(
     options?: IFindManyOptions<DeepPartial<Entity>>,
   ): Promise<Model[]> {
-    const query = this.getFindManyQuery(options);
+    const query = this.prepareFindManyQuery(options);
     return (await query.getMany()).map((entity) => {
       return this.mapper.toModel(entity);
     });
@@ -67,7 +58,7 @@ export abstract class BaseOrmRepository<Model, Entity> {
   public async findAndCount(
     options: IFindManyOptions<DeepPartial<Entity>>,
   ): Promise<IFindManyResult<Model>> {
-    const query = this.getFindManyQuery(options);
+    const query = this.prepareFindManyQuery(options);
     const { pageIdx } = options;
 
     const [list, totalCnt] = await query.getManyAndCount();
@@ -76,6 +67,7 @@ export abstract class BaseOrmRepository<Model, Entity> {
       data: list.map((item) => this.mapper.toModel(item)),
     };
   }
+
   public async findOneAndMetadata(
     options: IFindOneOptions<DeepPartial<Entity>>,
   ): Promise<IFindOneResult<Model>> {
@@ -84,4 +76,56 @@ export abstract class BaseOrmRepository<Model, Entity> {
       metadata: {},
     };
   }
+
+  protected prepareFindOneQuery(
+    options: IFindOneOptions<DeepPartial<Entity>>,
+  ): SelectQueryBuilder<Entity> {
+    const query = this.repository.createQueryBuilder();
+    const { where, andWhere, orderBy, andWhere2, having } = options;
+
+    if (having) {
+      query.having(having);
+    }
+
+    if (where) {
+      query.andWhere(where);
+    }
+    if (andWhere) {
+      query.andWhere(andWhere);
+    }
+    if (andWhere2) {
+      query.andWhere(andWhere2);
+    }
+    if (orderBy) {
+      query.addOrderBy(orderBy.sort, orderBy.order);
+    }
+    this.defineRelations(query);
+    return query;
+  }
+
+  protected defineRelations(queryBuilder: SelectQueryBuilder<Entity>) {}
+
+  protected prepareFindManyQuery(
+    options: IFindManyOptions<DeepPartial<Entity>>,
+  ): SelectQueryBuilder<Entity> {
+    return this.makeFindManyQuery(this.prepareFindOneQuery(options), options);
+  }
+
+  protected makeFindManyQuery = (
+    findOneQuery: SelectQueryBuilder<any>,
+    options: IFindManyOptions<any>,
+  ): SelectQueryBuilder<any> => {
+    const { pageIdx, pageSize, whereInIds, groupBy } = options;
+    if (whereInIds) {
+      findOneQuery.andWhereInIds(whereInIds);
+    }
+    if (pageIdx && pageSize) {
+      findOneQuery.skip(pageSize * (pageIdx - 1)).take(pageSize);
+    }
+    if (groupBy) {
+      findOneQuery.groupBy(groupBy);
+    }
+
+    return findOneQuery;
+  };
 }
--- src/user/infra/user.orm-repository.ts
+++ src/user/infra/user.orm-repository.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { BaseOrmRepository } from '../../common/base.orm-repository';
 import { User } from '../domain/user';
-import { IFindOneOptions } from '../../common/interface/interface';
-import { DeepPartial, Repository, SelectQueryBuilder } from 'typeorm';
+import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserOrmEntity } from './user.orm-entity';
 import { UserMapper } from './user.mapper';
@@ -13,11 +12,5 @@ export class UserOrmRepository extends BaseOrmRepository<User, UserEntity> {
     @InjectRepository(UserOrmEntity) repository: Repository<UserEntity>,
   ) {
     super(repository, new UserMapper());
-  }
-
-  protected getFindOneQuery(
-    options: IFindOneOptions<DeepPartial<UserEntity>>,
-  ): SelectQueryBuilder<UserEntity> {
-    return undefined;
   }
 }
