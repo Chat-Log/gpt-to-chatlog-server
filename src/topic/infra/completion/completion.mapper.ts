e9f6e5680197d93c25f1c9d4a665a356d950aec7
constwon111 <constwon111@gmail.com>
2023-04-11 20:38:05 +0900

    [Add] complete completion mapper.

--- src/topic/infra/completion/completion.mapper.ts
+++ src/topic/infra/completion/completion.mapper.ts
@@ -1,14 +1,45 @@
 import { Completion } from '../../domain/completion/completion';
 import { CompletionEntity } from '../../domain/completion/completion.entity';
+import { CompletionImpl } from './completion';
+import { chooseModel } from '../../../common/util/util';
 
 export class CompletionMapper
   implements BaseMapper<Completion, CompletionEntity>
 {
-  toEntity(model: Completion): CompletionEntity {
-    return undefined;
+  toEntity(completion: Completion): CompletionEntity {
+    if (!completion) return null;
+    const { id, answer, tokenCount, createdAt, updatedAt, model, question } =
+      completion.getPropsCopy();
+    return {
+      id,
+      answer,
+      tokenCount,
+      createdAt,
+      updatedAt,
+      modelName: model.getName(),
+      question,
+    };
   }
 
   toModel(entity: CompletionEntity): Completion {
-    return undefined;
+    if (!entity) return null;
+    const {
+      id,
+      answer,
+      tokenCount,
+      createdAt,
+      updatedAt,
+      modelName,
+      question,
+    } = entity;
+    return new CompletionImpl({
+      id,
+      answer,
+      tokenCount,
+      createdAt,
+      updatedAt,
+      model: chooseModel(modelName),
+      question,
+    });
   }
 }
