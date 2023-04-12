c24519118f8b3823c3c1b50311365daec9d5b500
constwon111 <constwon111@gmail.com>
2023-04-12 21:52:10 +0900

    [Feat] add mock model to test. - without it, i will keep paying to call api

--- src/common/util/util.ts
+++ src/common/util/util.ts
@@ -1,11 +1,14 @@
 import { ModelName } from '../enum/enum';
 import { ModelProvider } from '../../model-provider/model-provider';
 import { ChatGPTModel } from '../../ai-models/chat-gpt-model';
+import { MockModel } from '../../model-provider/mock-model';
 
 export const chooseModel = (model: ModelName): ModelProvider => {
   switch (model) {
     case ModelName['GPT3.5_TURBO']:
       return new ChatGPTModel();
+    case ModelName['MOCK']:
+      return new MockModel();
     default:
       return new ChatGPTModel();
   }
