ba7d94a5eff54e0a1ce44f4241eaf56c73c9ac9d
constwon111 <constwon111@gmail.com>
2023-04-30 17:56:06 +0900

    [Add] add code with swagger to controller and dto.

--- src/user/infra/dto/change-password.dto.ts
+++ src/user/infra/dto/change-password.dto.ts
@@ -1,14 +1,27 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ChangePasswordDto {
+  @ApiProperty({
+    example: 'old_password',
+    description: "The user's current password",
+  })
   @IsString()
   @IsNotEmpty()
   oldPassword: string;
 
+  @ApiProperty({
+    example: 'new_password',
+    description: "The user's new password",
+  })
   @IsString()
   @IsNotEmpty()
   newPassword: string;
 
+  @ApiProperty({
+    example: 'user_id',
+    description: 'The user ID of the user changing the password',
+  })
   @IsString()
   @IsNotEmpty()
   userId: string;
--- src/user/infra/dto/login-by-email.dto.ts
+++ src/user/infra/dto/login-by-email.dto.ts
@@ -1,10 +1,20 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginByEmailDto {
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    description: 'The email address of the user',
+    format: 'email',
+  })
   @IsString()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    example: 'password123',
+    description: 'The password for the user',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
