591819e3059183e2a2fd076f61461ee9c1f599e5
constwon111 <constwon111@gmail.com>
2023-05-22 13:15:48 +0900

    [Add] add new error to throw error when  user have invalid gpt key.

--- src/common/exception/internal.exception.ts
+++ src/common/exception/internal.exception.ts
@@ -2,11 +2,19 @@ import { BaseException } from './base.exception';
 import { ErrorStatusCode } from './error-status-code';
 
 class InternalException extends BaseException {
-  constructor(statusCode: ErrorStatusCode, message: string) {
+  constructor(
+    statusCode: ErrorStatusCode,
+    message: string = 'internal server exception',
+  ) {
     super(statusCode, message);
   }
 }
 
+export class InternalServerErrorCustomException extends InternalException {
+  constructor(message: string = 'internal server exception') {
+    super(ErrorStatusCode.INTERNAL_SERVER_ERROR, message);
+  }
+}
 export class UnknownException extends InternalException {
   constructor(message: string) {
     super(ErrorStatusCode.UNKNOWN_EXCEPTION, message);
